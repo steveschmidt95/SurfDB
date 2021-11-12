0813c4094b7d588245a11d6c1651c0a91dadaec0
STephen Schmidt <schmidt.st@husky.neu.edu>
2021-11-11 19:52:28 -0500

    fixed mydb error

--- InitTest.sql
+++ InitTest.sql
@@ -3,12 +3,36 @@ CREATE SCHEMA IF NOT EXISTS `surfdb`;
 USE `surfdb`;
 
 DROP TABLE IF EXISTS `surfdb`.`ReportDataPoint`;
+DROP TABLE IF EXISTS `surfdb`.`ReportWeather`;
+DROP TABLE IF EXISTS `surfdb`.`ReportImages`;
 
-CREATE TABLE ReportDataPoint (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
+CREATE TABLE IF NOT EXISTS `surfdb`.`ReportWeather` (
+    `ReportWeatherKey` BIGINT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`ReportWeatherKey`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `surfdb`.`ReportImages` (
+    `ReportImagesKey` BIGINT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`ReportImagesKey`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `surfdb`.`ReportDataPoint` (
+    `ReportDataPointKey` BIGINT NOT NULL AUTO_INCREMENT,
+    `ReportDate` DATE NOT NULL,
+    `ReportImagesFK` BIGINT NULL,
+	`ReportWeatherFK` BIGINT NULL,
+    PRIMARY KEY (`ReportDataPointKey`),
+    INDEX `fk_report_images_idx` (`ReportImagesFK` ASC) VISIBLE,
+	INDEX `fk_report_weather_idx` (`ReportWeatherFK` ASC) VISIBLE,
+	CONSTRAINT `fk_report_images_idx`
+	  FOREIGN KEY (`ReportImagesFK`)
+      REFERENCES `mydb`.`ReportImages` (`ReportImagesKey`)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	CONSTRAINT `fk_report_weather_idx`
+	  FOREIGN KEY (`ReportWeatherFK`)
+	  REFERENCES `mydb`.`ReportWeather` (`ReportWeatherKey`)
+	  ON DELETE NO ACTION
+	  ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
