108dffdb35e3737d003da8e34419fe36bbf08d4f
STephen Schmidt <stephen.p.schmidt@uconn.edu>
2022-03-03 15:09:03 -0500

    bug fix

--- src/ImageCollector.py
+++ src/ImageCollector.py
@@ -16,11 +16,11 @@ class ImageCollector(DataCollector):
 
     def collect_set_number_images_per_hour(self, num_images_per_hour=30):
 
-        assert 3600 % num_images_per_hour == 0 # Keep it divisible by number of seconds in an hour for now until that can be fixed
+        assert 3600 % num_images_per_hour == 0  # Keep it divisible by number of seconds in an hour for now until that can be fixed
 
         current_day = date.today()
         current_day_image_path = os.path.join(self.image_experiment_folder, str(current_day))
-        os.makedirs(current_day_image_path , exist_ok=True)
+        os.makedirs(current_day_image_path, exist_ok=True)
         image_collection_time_frame = 3600 / num_images_per_hour
 
         # Outer loop, collects until the specified date
@@ -38,9 +38,12 @@ class ImageCollector(DataCollector):
                 current_hour = datetime.now().hour
                 current_minute = datetime.now().minute
                 current_seconds_in_hour = datetime.now().second + current_minute * 60
+                print('Current Seconds: ', current_seconds_in_hour)
+                print('Next Collection Time: ', next_image_acquisition_time_in_seconds)
                 while current_seconds_in_hour < next_image_acquisition_time_in_seconds:
                     time.sleep(.5)
-                    current_seconds_in_hour = datetime.now().second + current_minute * 60
+                    current_seconds_in_hour = datetime.now().second + datetime.now().minute * 60
+
 
                 # Wait over, grab frame, reset times
                 im_filename = os.path.join(current_day_image_path, str(current_hour) + '_'
@@ -49,12 +52,16 @@ class ImageCollector(DataCollector):
                 current_hour = datetime.now().hour
                 images_collected_current_hour += 1
 
+                print('Hour: ', current_hour, '||', 'Images Collected: ', images_collected_current_hour)
+
 
 end_date = date(2022, 3, 5)
 daylight_start_hour = 5
-daylight_end_hour = 7+12 # 24 hour time, this would be 7 PM
+daylight_end_hour = 7 + 12  # 24 hour time, this would be 7 PM
 experiment_data_folder = 'init_data_test'
 video_url = 'https://www.youtube.com/watch?v=NqhAaA2mGcA'
 im_collect = ImageCollector(end_date, daylight_start_hour, daylight_end_hour, experiment_data_folder, video_url)
 
-im_collect.collect_set_number_images_per_hour()
+images_per_hour = 60
+
+im_collect.collect_set_number_images_per_hour(images_per_hour)
